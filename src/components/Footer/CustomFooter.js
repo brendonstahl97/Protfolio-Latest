c8df308138c7b3b0030bc1fad572cfe9d399f059
brendonstahl97 <brendonstahl97@gmail.com>
2021-03-17 15:59:01 -0400

    added responsive social media links

--- src/components/Footer/CustomFooter.js
+++ src/components/Footer/CustomFooter.js
@@ -8,9 +8,7 @@ import {
 export default function CustomFooter() {
     return (
         <footer className='customFooter'>
-
-
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 290">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#0099ff" fillOpacity="1" d="M0,288L48,288C96,288,192,288,288,256C384,224,480,160,576,122.7C672,85,768,75,864
                 ,96C960,117,1056,171,1152,176C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,
                 960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
@@ -22,14 +20,16 @@ export default function CustomFooter() {
                         <Col className='d-flex justify-content-end'>
                             <a href='https://www.linkedin.com/in/brendon-stahl-9a518a176/' rel="noreferrer" target="_blank" style={{textDecoration: "none"}}>
                                 <div className='d-flex align-items-center justify-content-center socialBox'>
-                                    <i className="fab fa-linkedin-in fa-4x"></i>
+                                    <i className="fab fa-linkedin-in fa-3x d-sm-none"></i>
+                                    <i className="fab fa-linkedin-in fa-4x d-none d-sm-block"></i>
                                 </div>
                             </a>
                         </Col>
                         <Col className='d-flex justify-content-start'>
                             <a href='https://github.com/brendonstahl97' rel="noreferrer" target="_blank" style={{textDecoration: "none"}}>
                                 <div className='d-flex align-items-center justify-content-center socialBox'>
-                                    <i className="fab fa-github fa-4x"></i>
+                                    <i className="fab fa-github fa-3x d-sm-none"></i>
+                                    <i className="fab fa-github fa-4x d-none d-sm-block"></i>
                                 </div>
                             </a>
                         </Col>
