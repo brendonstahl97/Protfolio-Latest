bea40a5c53f2399f06ca529d0802a09752acca56
brendonstahl97 <brendonstahl97@gmail.com>
2021-03-17 16:14:57 -0400

    Slight Change to link styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar/CustomNavbar';
 import Footer from './components/Footer/CustomFooter';
+import Landing from './views/Landing';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+        <Landing />
       <Footer />
     </div>
   );
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Landing() {
     return (
         <div>
-            
+            <h1>Brendon Stahl</h1>
+            <h3>Full Stack Web Developer
+            </h3>
         </div>
     );
-}
+};
